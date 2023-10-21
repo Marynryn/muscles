9afdccad95c71b924c9d6bee51e69f58458803c2
Marynryn <132609093+Marynryn@users.noreply.github.com>
2023-10-21 17:02:06 +0000

    Deploying to gh-pages from @ Marynryn/muscles@fc3908386b0d6d50918dace226b570b477d375f9 🚀

---
+++ assets/index-608f294f.js
@@ -0,0 +1,1 @@
+import"./styles-1b35a218.js";function t(){const o=document.getElementById("scrollToTopButton");document.body.scrollTop>20||document.documentElement.scrollTop>20?o.style.display="block":o.style.display="none"}document.getElementById("scrollToTopButton").addEventListener("click",function(){document.body.scrollTop=0,document.documentElement.scrollTop=0});window.onscroll=t;
