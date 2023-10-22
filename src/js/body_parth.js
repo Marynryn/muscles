9079154c7b7c171ff4ac24ab97fbd5fef5583e5c
k0robova <alonakorobova@ukr.net>
2023-10-23 00:01:42 +0300

    section-exercises

--- src/js/body_parth.js
+++ src/js/body_parth.js
@@ -1,82 +1,256 @@
+import { getData, patchData, postData } from './api.js';
+import axios from 'axios';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
+
 const refs = {
   exercisesList: document.querySelector('.exercises-list'),
-  //   btnBodyParts: document.querySelector('.js-btn-body-parts'),
-  //   btnMuscles: document.querySelector('.js-btn-muscles'),
-  //   btnEquipment: document.querySelector('.js-btn-equipment'),
-
-  inputSearch: document.querySelector('#exercises-input'),
+  exercisesForm: document.querySelector('.exercises-form'),
   listPhotoCard: document.querySelector('.js-list-cards-photo'),
-  listInfoCard: document.querySelector('.js-list-cards-info'),
-
   mainTitle: document.querySelector('.exercises-title'),
+  spanTitle: document.querySelector('.title-span-exercise'),
+  divInputButton: document.querySelector('.input-button'),
 };
 
 refs.exercisesList.addEventListener('click', onBtnClick);
+let lastSelectedSectionBtn = undefined;
+let lastSelectedCardTitle = undefined;
+
+let isMainScreen = true;
 
 function onBtnClick(evt) {
-  if (!evt.target.tagName === 'BUTTON') {
+  if (evt.target.tagName !== 'BUTTON') {
     return;
   }
+  refs.mainTitle.textContent = 'Exercises';
+  refs.spanTitle.textContent = '';
+  refs.exercisesForm.style.display = 'none';
 
-  //   // ! Запит
-  //   fetch(
-  //     'https://your-energy.b.goit.study/api/exercises?bodypart=waist&page=1&limit=10'
-  //   )
-  //     .then(res => res.json())
-  //     .then(
-  //       ({ results }) =>
-  //         (refs.listInfoCard.innerHTML = createMarkupCardInfo(results))
-  //     );
+  if (lastSelectedSectionBtn !== undefined) {
+    lastSelectedSectionBtn.classList.remove('current');
+  }
 
-  // Запит 2
-  fetch(
-    'https://your-energy.b.goit.study/api/filters?filter=Muscles&page=1&limit=12'
-  )
-    .then(res => res.json())
-    .then(
-      ({ results }) =>
-        (refs.listPhotoCard.innerHTML = createMarkupCardPhoto(results))
+  lastSelectedSectionBtn = evt.target;
+  evt.target.classList.add('current');
+
+  //   console.log(lastSelectedSectionBtn, 'CONSOLE');
+
+  startApi(1, evt.target.textContent).then(
+    ({ results, page, perPage, totalPages }) => {
+      //   console.log(page, perPage, totalPages);
+
+      makePagination(
+        Number(page),
+        Number(perPage),
+        totalPages,
+        evt.target.textContent
+      );
+
+      refs.listPhotoCard.innerHTML = createMarkupCardPhoto(results);
+    }
+  );
+
+  refs.listPhotoCard.addEventListener('click', onItemClick);
+}
+
+let width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+let maxCardOnScreen = 12;
+
+if (width === 375) {
+  maxCardOnScreen = 9;
+  //   console.log(maxCardOnScreen, 'lol');
+}
+window.addEventListener('resize', onResize);
+
+function onResize() {
+  if (width === 375) {
+    maxCardOnScreen = 9;
+    console.log(maxCardOnScreen, 'lol');
+  }
+  console.log('Window width:', width);
+}
+
+async function startApi(page, text) {
+  try {
+    const params = {
+      endpoint: 'filters',
+      page: page,
+      limit: maxCardOnScreen,
+      filter: text,
+    };
+    return await getData(params);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function makePagination(page, perPage, totalPages, text) {
+  const pagination = new Pagination('pagination', {
+    totalItems: perPage * totalPages, // Total number of items
+    itemsPerPage: perPage, // Items per page
+    visiblePages: 3, // Visible pages in the pagination bar
+    page: page, // Current page
+  });
+  //   console.log(results);
+
+  pagination.on('afterMove', function (event) {
+    if (!isMainScreen) {
+      fetchExercisesDetails(
+        lastSelectedSectionBtn.dataset.id,
+        lastSelectedCardTitle,
+        pagination.getCurrentPage()
+      ).then(({ results, page, perPage, totalPages }) => {
+        console.log(page, perPage, totalPages);
+
+        makePagination(Number(page), Number(perPage), totalPages, text);
+        refs.listPhotoCard.innerHTML = createMarkupCardInfo(results);
+        console.log('helllllo');
+      });
+      return;
+    }
+    console.log('Page changed to:', event.page);
+
+    // Do something when the page changes
+    startApi(event.page, text).then(
+      ({ results, page, perPage, totalPages }) => {
+        console.log(page, perPage, totalPages);
+
+        makePagination(Number(page), Number(perPage), totalPages, text);
+
+        refs.listPhotoCard.innerHTML = createMarkupCardPhoto(results);
+      }
     );
+  });
 }
 
-refs.listInfoCard.addEventListener('click', onItemClick);
+// ! на картинку клік - перекидає на карточку з інформацією
 
-// function onItemClick(evt) {
-//   if (evt.target.tagName === 'LI') {
-//     fetch(
-//       'https://your-energy.b.goit.study/api/exercises?bodypart=waist&page=1&limit=10'
-//     )
-//       .then(res => res.json())
-//       .then(({ results }) => {
-//         refs.listPhotoCard.innerHTML = '';
-//         refs.listInfoCard.innerHTML = createMarkupCardInfo(results);
-//       });
-//   }
+// if (eventListenerActive) {
+//   console.log(eventListenerActive, 'EVENTTTTTT');
+refs.listPhotoCard.addEventListener('click', onItemClick);
 // }
+
 function onItemClick(evt) {
-  if (evt.target.tagName === 'LI') {
+  if (evt.target.tagName === 'UL') {
+    console.log(evt.target.tagName);
     return;
   }
-  fetch(
-    'https://your-energy.b.goit.study/api/exercises?bodypart=waist&page=1&limit=10'
-  )
-    .then(res => res.json())
-    .then(({ results }) => {
-      refs.listPhotoCard.innerHTML = '';
-      refs.listInfoCard.innerHTML = createMarkupCardInfo(results);
-    });
+
+  refs.exercisesForm.style.display = 'block';
+  isMainScreen = false;
+
+  refs.mainTitle.textContent = refs.mainTitle.textContent.replace(' / ', '');
+  const sectionKey = lastSelectedSectionBtn.dataset.id;
+
+  const sectionValue = evt.target.closest('.js-card-photo-item').dataset.name;
+
+  lastSelectedCardTitle = sectionValue;
+
+  refs.mainTitle.textContent += ' / ';
+  refs.spanTitle.textContent = `${sectionValue}`;
+
+  //   async function startApi(key, value) {
+  //     try {
+  //       let params = {
+  //         page: 1,
+  //         limit: 12,
+  //         endpoint: 'exercises',
+  //         // bodypart: 'back',
+  //         // muscles: 'lats',
+  //         // equipment: 'barbell',
+  //         // keyword: 'pull',
+  //       };
+  //       params[key] = value;
+
+  //       return await getData(params);
+  //     } catch (e) {
+  //       console.log(e);
+  //     }
+  //   }
+  fetchExercisesDetails(sectionKey, sectionValue, 1).then(
+    ({ results, page, perPage, totalPages }) => {
+      makePagination(
+        Number(page),
+        Number(perPage),
+        totalPages,
+        evt.target.textContent
+      );
+      refs.listPhotoCard.innerHTML = createMarkupCardInfo(results);
+    }
+  );
+
+  refs.listPhotoCard.removeEventListener('click', onItemClick);
+}
+async function fetchExercisesDetails(key, value, page) {
+  try {
+    let params = {
+      page: page,
+      limit: 12,
+      endpoint: 'exercises',
+      // bodypart: 'back',
+      // muscles: 'lats',
+      // equipment: 'barbell',
+      // keyword: 'pull',
+    };
+    params[key] = value;
+
+    return await getData(params);
+  } catch (e) {
+    console.log(e);
+  }
+}
+// ! Інпут
+refs.exercisesForm.addEventListener('submit', onInputSearch);
+
+function onInputSearch(evt) {
+  evt.preventDefault();
+
+  const inputValue = evt.currentTarget.elements.target.value;
+  //   console.log(inputValue);
+
+  fetchExercisesByKeyword(inputValue).then(({ results }) => {
+    if (results.length === 0) {
+      console.log('За вашим запитом нічого не знайдено :( Спробуйте ще раз!');
+    }
+    refs.listPhotoCard.innerHTML = createMarkupCardInfo(results);
+    refs.exercisesForm.reset();
+  });
+}
+// ! API
+async function fetchExercisesByKeyword(inputValue) {
+  try {
+    let params = {
+      page: 1,
+      limit: 12,
+      endpoint: 'exercises',
+      // bodypart: 'back',
+      // muscles: 'lats',
+      // equipment: 'barbell',
+      keyword: `${inputValue}`,
+    };
+    console.log(lastSelectedSectionBtn.dataset.id, 'LOooooool');
+    params[lastSelectedSectionBtn.dataset.id] = lastSelectedCardTitle;
+    console.log(params, 'params');
+    return await getData(params);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
+// ! 2 Функції розмітки
 function createMarkupCardInfo(arr) {
   return arr
     .map(
       ({ _id, bodyPart, name, burnedCalories, time, rating, target }) => `
       
-<li class="card-info-item" data-id="${_id}">
+ <li class="card-info-item" data-id="${_id}">
   <div class="div-icon-workout">
     <div class="div-workout-rating">
       <a class="link-workout">workout</a>
-      <p class="js-text">
+      <p class="js-text-rating">
         ${rating}
         <svg class="icon-star" width="18" height="18">
           <use href="../img/icons.svg#icon-star"></use>
@@ -84,38 +258,41 @@ function createMarkupCardInfo(arr) {
       </p>
     </div>
     <button type="button" class="btn-start">
-     
       Start
       <svg class="icon-arrow" width="16" height="16">
         <use href="../img/icons.svg#icon-arrow"></use>
       </svg>
     </button>
   </div>
-  <h3 class="fav-title">
-    <svg class="fav-icon-running" width="24" height="24">
+
+  <h3 class="exercise-title">
+    <svg class="exercise-icon-running" width="24" height="24">
       <use href="../img/icons.svg#icon-running-stick" height="16" y="4"></use>
     </svg>
     ${name}
   </h3>
 
-  <ul class="js-fav-list">
-    <li class="fav-list-text">
-      <p class="fav-label">
+  <ul class="js-exercise-list">
+
+    <li class="exercise-list-text">
+      <p class="exercise-label">
         Burned calories:
-        <span class="fav-text-span">
+        <span class="exercise-text-span">
           
           ${burnedCalories}/${time}min
         </span>
       </p>
     </li>
-    <li class="fav-list-text">
-      <p class="fav-label">
-        Body part: <span class="fav-text-span"> ${bodyPart}</span>
+
+    <li class="exercise-list-text">
+      <p class="exercise-label">
+        Body part: <span class="exercise-text-span"> ${bodyPart}</span>
       </p>
     </li>
-    <li class="fav-list-text">
-      <p class="fav-label">
-        Target: <span class="fav-text-span"> ${target}</span>
+
+    <li class="exercise-list-text">
+      <p class="exercise-label">
+        Target: <span class="exercise-text-span"> ${target}</span>
       </p>
     </li>
   </ul>
@@ -128,42 +305,16 @@ function createMarkupCardPhoto(arr) {
   return arr
     .map(
       ({ filter, name, imgURL }) =>
-        `<li class="js-card-photo-item"   style="background-image: linear-gradient(0deg, rgba(17, 17, 17, 0.50) 0%, rgba(17, 17, 17, 0.50) 100%), url(${imgURL});">
+        `<li class="js-card-photo-item"  data-name="${name}"  style="background-image: linear-gradient(0deg, rgba(17, 17, 17, 0.50) 0%, rgba(17, 17, 17, 0.50) 100%), url(${imgURL});">
              <div class="js-card-text">
-              <button type="button" class="js-title-card-photo" style=" border: none;
-                                                                        background-color: transparent;">${
-                                                                          name
-                                                                            .charAt(
-                                                                              0
-                                                                            )
-                                                                            .toUpperCase() +
-                                                                          name.slice(
-                                                                            1
-                                                                          )
-                                                                        }
-             </button>
-              <p class ="js-text-card-photo">${filter}</p>
+                <p class="js-title-card-photo" style=" border: none;
+                    background-color: transparent;">${
+                      name.charAt(0).toUpperCase() + name.slice(1)
+                    }
+               </p>
+               <p class ="js-text-card-photo">${filter}</p>
              </div>
           </li>`
     )
     .join('');
 }
-// 2 Функції розмітки
-// function createMarkupCardPhoto(arr) {
-//   return arr
-//     .map(
-//       ({ filter, name, imgURL }) =>
-//         `<li class="js-card-photo-item" >
-//           <img src="${imgURL}" alt="${
-//           name.charAt(0).toUpperCase() + name.slice(1)
-//         }" width="335" height="225" class="js-card-photo" style="background-image: linear-gradient(0deg, rgba(17, 17, 17, 0.50) 0%, rgba(17, 17, 17, 0.50) 100%), url(${imgURL})">
-//            <div class="js-card-text">
-//             <h3 class="js-title-card-photo">${
-//               name.charAt(0).toUpperCase() + name.slice(1)
-//             }</h3>
-//             <p class ="js-text-card-photo">${filter}</p>
-//            </div>
-//         </li>`
-//     )
-//     .join('');
-// }
