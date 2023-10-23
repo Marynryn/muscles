e653476ad928ba355edcf46fc0359ef8b73ff5ed
Marynryn <murmar95@gmail.com>
2023-10-23 16:41:15 +0300

    modal+body-parts

--- src/js/body_parth.js
+++ src/js/body_parth.js
@@ -183,18 +183,17 @@ function onItemClick(evt) {
       // for Olia ============================================
        modalStartBtn = document.querySelectorAll('.btn-start');
       console.log(modalStartBtn, 'ModalStartBTn11111');
-      // modalStartBtn.forEach(button =>
-      // button.addEventListener('click', onModalTestBtn));
+      modalStartBtn.forEach( button =>
+        button.addEventListener('click', closeOpenModal))
     }
   );
   refs.listPhotoCard.removeEventListener('click', onItemClick);
 }
 
-// // !! TESTTESTEST
-// function onModalTestBtn() {
-//   console.log('HEELLLLLLLLOooooooooo');
-// }
-
+function closeOpenModal() { console.log("hiiii");
+    modalWindow.classList.toggle('is-hidden');
+   
+}
 async function fetchExercisesDetails(key, value, page) {
   try {
     let params = {
@@ -332,3 +331,61 @@ function createMarkupCardPhoto(arr) {
 
  export let modalStartBtn = [];
 console.log(modalStartBtn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MODAL/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// ! open and close modal window
+const closeModalBtn = document.querySelector('[data-modal-close]');
+const modalWindow = document.querySelector(".modal-section")
+modalStartBtn.forEach( button =>
+    button.addEventListener('click', closeOpenModal))
+closeModalBtn.addEventListener('click', closeOpenModal)
+// modalStartBtn.addEventListener('click', closeOpenModal)
+
+// function closeOpenModal() { console.log("hiiii");
+//     modalWindow.classList.toggle('is-hidden');
+   
+// }
+
+
+// ! add exercise in favorites
+const btnFavorites = document.querySelector(".btn-add-to-favorites")
+btnFavorites.addEventListener('click', addToFavorites)
+
+function isDataInLocalStorage(key) {
+    return localStorage.getItem(key) !== null;
+}
+
+function addToFavorites() {
+    const dataToSave = 'X';
+    localStorage.setItem('keyX', dataToSave);
+
+    const keyInLocalStorage = isDataInLocalStorage('keyX');
+    if (keyInLocalStorage) { 
+        btnFavorites.classList.add('on-click-btn');
+        btnFavorites.textContent = 'This exercise in your Favorites';
+    }
+}
+
+// ! rating stars
+const stars = document.querySelectorAll('.star');
+const ratingStars = document.querySelector('.rating_value');
+
+const ratingValue = Math.round(parseFloat(ratingStars.textContent));
+
+stars.forEach((star, index) => {
+    if (index < ratingValue) {
+        star.classList.add('active-star'); 
+    }
+});
