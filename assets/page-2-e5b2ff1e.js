61820dfdc96b572e5d0db163a00069c663a453bd
Marynryn <132609093+Marynryn@users.noreply.github.com>
2023-10-24 06:35:23 +0000

    Deploying to gh-pages from @ Marynryn/muscles@26d77d7485bc9a292f83c99409fee0f5421c23f7 🚀

--- assets/page-2-e5b2ff1e.js
+++ assets/page-2-e5b2ff1e.js
@@ -1,7 +1,9 @@
-import"./background-a188bcc8.js";const s={list:document.querySelector(".fav-list"),text:document.querySelector(".fav-text")};s.list.addEventListener("click",r);s.list.addEventListener("click",f);fetch("https://your-energy.b.goit.study/api/exercises?bodypart=waist&page=1&limit=10").then(t=>t.json()).then(({results:t})=>{s.text.classList.add("is-hidden"),s.list.innerHTML=o(t)});function o(t){return t.map(({name:e,bodyPart:i,target:a,time:n,burnedCalories:l,_id:c})=>`
+import"./background-7429213a.js";const s={list:document.querySelector(".fav-list"),text:document.querySelector(".fav-text")};s.list.addEventListener("click",r);fetch("https://your-energy.b.goit.study/api/exercises?bodypart=waist&page=1&limit=10").then(t=>t.json()).then(({results:t})=>{s.text.classList.add("is-hidden"),s.list.innerHTML=o(t)});function o(t){return t.map(({name:e,bodyPart:i,target:a,time:n,burnedCalories:l,_id:c})=>`
     <li class="fav-item" id='${c}'>
             <div class="fav-box-link">
-                <a href="./index.html" class="fav-link">WORKOUT</a>
+
+                <a href="./index.html" class="fav-link">workout</a>
+
                 <button class="fav-btn-delete">
                 <svg  width="16" height="16">
                     <use class="fav-icon-trach" href="./img/icons.svg#icon-trash"></use>
@@ -18,24 +20,24 @@ import"./background-a188bcc8.js";const s={list:document.querySelector(".fav-list
                 <svg class="fav-icon-running" width="24" height="24">
                     <use href="./img/icons.svg#icon-running-stick" y="4" height="16" ></use>
                 </svg>
-                ${e.charAt(0).toUpperCase()+e.slice(1)}
+                <span>${e.charAt(0).toUpperCase()+e.slice(1)}</span>
             </h3>
             <ul class="fav-list-text">
                 <li class="fav-item-text">
                     <p class="fav-label">Burned calories:
-                        <span class="fav-text-span">${l} / ${n}</span>
+                        <span class="fav-text-span">${l} / ${n} min</span>
                     </p>
                 </li>
                 <li class="fav-item-text">
                     <p class="fav-label">Body part:
-                        <span class="fav-text-span">${i}</span>
+                        <span class="fav-text-span">${i.charAt(0).toUpperCase()+i.slice(1)}</span>
                     </p>
                 </li>
                 <li class="fav-item-text">
-                    <p class="fav-label">Target:
-                        <span class="fav-text-span">${a}</span>
+                    <p class="fav-label text-width-elipses">Target:
+                        <span class="fav-text-span">${a.charAt(0).toUpperCase()+a.slice(1)}</span>
                     </p>
                 </li>
             </ul>
         </li>
-    `).join("")}function r(t){if(!t.target.classList.contains("fav-icon-trach"))return;const e=t.target.closest(".fav-item");if(e){const i=e.id;document.getElementById(i).remove()}v()}function v(){s.list.childElementCount===0&&s.text.classList.remove("is-hidden")}function f(){}
+    `).join("")}function r(t){if(!t.target.closest(".fav-btn-delete")){console.log(123);return}const e=t.target.closest(".fav-item");if(e){const i=e.id;document.getElementById(i).remove()}v()}function v(){s.list.childElementCount===0&&s.text.classList.remove("is-hidden")}
